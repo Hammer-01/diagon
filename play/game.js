7bd2a15e98d25b9f2c72b792d7fb6a6b2707c164
Lachlan Hammersley <lachlanh04@gmail.com>
2023-11-24 19:23:32 +1000

    Add board representation and make size properly modifiable

--- play/game.js
+++ play/game.js
@@ -1,8 +1,12 @@
 function Diagon(boardWidth, boardHeight, boardColours, numTiles) {
-    this.numTiles = numTiles ?? 6;
-    this.ts = width / this.numTiles; // diagonal length (tileSize)
+    this.numTiles = numTiles ?? 10;
+    this.ts = width / ((this.numTiles + 2) / 2); // diagonal length (tileSize)
     this.bw = boardWidth ?? width;
     this.bh = boardHeight ?? height;
+
+    // distances for centre square
+    this.c1 = (this.numTiles + 2) / 4;
+    this.c2 = this.c1 - 1;
 
     this.boardColours = boardColours ?? {
         primary: "#964b00",
@@ -10,9 +14,17 @@ function Diagon(boardWidth, boardHeight, boardColours, numTiles) {
         tertiary: "#190d00",
     };
     
-    this.board = [
-        {}
-    ];
+    this.board = [...Array(this.numTiles)].map(() => Array(this.numTiles));
+    for (i = 0; i < this.numTiles/2; i++) {
+        for (j = 0; j < this.numTiles/2; j++) {
+            let isWall = j < this.numTiles/2 - 1 - i;
+            this.board[i][j] = new Tile(isWall);
+            this.board[i][this.numTiles-1-j] = new Tile(isWall);
+            this.board[this.numTiles-1-i][j] = new Tile(isWall);
+            this.board[this.numTiles-1-i][this.numTiles-1-j] = new Tile(isWall);
+        }
+    }
+    console.log(this.board);
 }
 
 Diagon.prototype.drawBoard = function() {
@@ -20,11 +32,15 @@ Diagon.prototype.drawBoard = function() {
     noStroke();
     fill(this.boardColours.primary);
     square(0, 0, this.bw);
+
+    // dark x
     fill(this.boardColours.secondary);
     quad(0, this.ts, this.ts, 0, this.bw, this.bh-this.ts, this.bw-this.ts, this.bh);
     quad(this.bw, this.ts, this.bw-this.ts, 0, 0, this.bh-this.ts, this.ts, this.bh);
+
+    // centre square
     fill(this.boardColours.tertiary);
-    quad(2*this.ts, 3*this.ts, 3*this.ts, 2*this.ts, this.bw-2*this.ts, this.bh-3*this.ts, this.bw-3*this.ts, this.bh-2*this.ts);
+    quad(this.c1*this.ts, this.c2*this.ts, this.c2*this.ts, this.c1*this.ts, this.bw-this.c1*this.ts, this.bh-this.c2*this.ts, this.bw-this.c2*this.ts, this.bh-this.c1*this.ts);
 
     // gridlines
     stroke('black');
@@ -57,4 +73,10 @@ Diagon.prototype.getPosition = function() {
 
 Diagon.prototype.setPosition = function() {
 
+}
+
+
+function Tile(isWall) {
+    this.value = isWall === true ? 'wall' : ''; // TODO: rename value to a better name
+    // TODO: add dark as possible value
 }
